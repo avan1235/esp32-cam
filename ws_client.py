6331effacf3969fd93b2ec55620fb91de00054b2
avan1235 <macieekprocyk@gmail.com>
2022-11-11 00:40:53 +0100

    make working sending images

--- ws_client.py
+++ ws_client.py
@@ -1,4 +1,10 @@
+import io
+
 import websocket
+import tkinter as tk
+from PIL import Image, ImageTk
+import cv2
+import os
 
 OPCODE_TEXT = 0x1
 OPCODE_BIN = 0x2
@@ -12,17 +18,17 @@ class WsClient:
         self.ip = ip
         self.ws = websocket.WebSocket()
 
-    def __enter__(self):    # type: ignore
+    def __enter__(self):  # type: ignore
         self.ws.connect('ws://{}:{}/ws'.format(self.ip, self.port))
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):     # type: ignore
+    def __exit__(self, exc_type, exc_value, traceback):  # type: ignore
         self.ws.close()
 
-    def read(self):     # type: ignore
+    def read(self):  # type: ignore
         return self.ws.recv_data(control_frame=True)
 
-    def write(self, data='', opcode=OPCODE_TEXT):   # type: ignore
+    def write(self, data='', opcode=OPCODE_TEXT):  # type: ignore
         if opcode == OPCODE_BIN:
             return self.ws.send_binary(data.encode())
         if opcode == OPCODE_PING:
@@ -33,6 +39,14 @@ class WsClient:
 if __name__ == "__main__":
     ip = "192.168.4.1"
     port = 80
+    window = tk.Tk()
+
+    # window.geometry("500x500") # (optional)
+
     with WsClient(ip, port) as ws:
-        ws.write("Trigger async", opcode=OPCODE_TEXT)
-        print(ws.read())
+        ws.write("start", opcode=OPCODE_TEXT)
+        for i in range(10):
+            code, data = ws.read()
+            bytes_io = io.BytesIO(data)
+            with Image.open(bytes_io, formats=["jpeg"]) as img:
+                img.save(f"{i}.jpg")
