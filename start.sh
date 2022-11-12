6ea7259ab0d54d95f8f3200701b65f903bcd19d0
avan1235 <macieekprocyk@gmail.com>
2022-11-13 00:25:03 +0100

    create image for development env

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ssh-keygen -f "/home/avan1235/.ssh/known_hosts" -R "[localhost]:2222"
+docker build -t avan1235/espressif-idf-camera:0.1 -f Dockerfile .
+docker stop espressif_idf_camera
+docker rm espressif_idf_camera
+docker run -d --cap-add sys_ptrace -p 127.0.0.1:2222:22 --device=/dev/ttyUSB0 --name espressif_idf_camera avan1235/espressif-idf-camera:0.1
