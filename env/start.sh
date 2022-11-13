0f296b1ed9bde8d58035d25c3bfc5aba76c601cf
avan1235 <macieekprocyk@gmail.com>
2022-11-13 10:56:17 +0100

    add option to run with no device

--- env/start.sh
+++ env/start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+CONTAINER_NAME="espressif_idf_camera"
+IMAGE_TAG="avan1235/espressif-idf-camera:0.1"
+
 ssh-keygen -f "/home/avan1235/.ssh/known_hosts" -R "[localhost]:2222"
-docker build --build-arg UID="$(id -u)" -t avan1235/espressif-idf-camera:0.1 -f Dockerfile .
-docker stop espressif_idf_camera
-docker rm espressif_idf_camera
-docker run -d --cap-add sys_ptrace -p 127.0.0.1:2222:22 --device=/dev/ttyUSB0 --name espressif_idf_camera avan1235/espressif-idf-camera:0.1
+docker build --build-arg UID="$(id -u)" -t "${IMAGE_TAG}" -f Dockerfile .
+docker stop "${CONTAINER_NAME}"
+docker rm "${CONTAINER_NAME}"
+if [[ "$*" == *"--no-device"* ]]
+then
+    docker run -d --cap-add sys_ptrace -p 127.0.0.1:2222:22 --name "${CONTAINER_NAME}" "${IMAGE_TAG}"
+else
+    docker run -d --cap-add sys_ptrace -p 127.0.0.1:2222:22 --device=/dev/ttyUSB0 --name "${CONTAINER_NAME}" "${IMAGE_TAG}"
+fi
