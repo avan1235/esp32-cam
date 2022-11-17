8df01f3ab9002f066208e1b2319bc59a6aac8ece
avan1235 <macieekprocyk@gmail.com>
2022-11-17 17:11:48 +0100

    update camera client to be self contained

--- client/camera.py
+++ client/camera.py
@@ -9,52 +9,25 @@ OPCODE_PING = 0x9
 OPCODE_PONG = 0xa
 
 
-class WsClient:
-    def __init__(self, ip, port, path):
-        self.port = port
-        self.ip = ip
-        self.path = path
+class Camera:
+    def __init__(self, ip="192.168.4.1", port=80):
+        def control_url(path):
+            requests.put(f"http://{ip}:{port}/control{path}")
+
+        self.control = control_url
+        self.ws_url = f"ws://{ip}:{port}/video"
         self.ws = websocket.WebSocket()
 
     def __enter__(self):
-        self.ws.connect(f"ws://{self.ip}:{self.port}{self.path}")
+        self.ws.connect(self.ws_url)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.ws.close()
 
-    def read(self):
-        try:
-            return self.ws.recv_data()
-        except:
-            return None, None
-
-    def write(self, data, opcode=OPCODE_TEXT):
-        if opcode == OPCODE_BIN:
-            return self.ws.send_binary(data.encode())
-        if opcode == OPCODE_PING:
-            return self.ws.ping(data)
-        return self.ws.send(data)
-
-
-class Camera:
-    def __init__(self, camera_ip="192.168.4.1", recv_port=80):
-        def control_url(path):
-            requests.put(f"http://{camera_ip}:{recv_port}/control{path}")
-
-        self.control = control_url
-        self.video = WsClient(camera_ip, recv_port, "/video")
-
-    def __enter__(self):
-        self.video = self.video.__enter__()
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        self.video.__exit__(*args, **kwargs)
-
     def get_frame(self):
-        self.video.write("s")
-        _, data = self.video.read()
+        self.__ws_write("s")
+        _, data = self.__ws_read()
         if data is None:
             return None
         array = np.frombuffer(data, dtype=np.uint8)
@@ -68,3 +41,16 @@ class Camera:
 
     def switch_flash_led(self):
         self.control("/led/switch")
+
+    def __ws_write(self, data, opcode=OPCODE_TEXT):
+        if opcode == OPCODE_BIN:
+            return self.ws.send_binary(data.encode())
+        if opcode == OPCODE_PING:
+            return self.ws.ping(data)
+        return self.ws.send(data)
+
+    def __ws_read(self):
+        try:
+            return self.ws.recv_data()
+        except:
+            return None, None
