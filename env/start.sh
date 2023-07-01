0a490523258844e73543cbf4df30671b09baa2e2
avan1235 <macieekprocyk@gmail.com>
2023-07-01 18:14:40 +0200

    update env script

--- env/start.sh
+++ env/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 CONTAINER_NAME="espressif_idf_camera"
-IMAGE_TAG="avan1235/espressif-idf-camera:latest"
+IMAGE_TAG="avan1235/espressif-idf-camera:sha-f2d84aef94cb5f00701332841b7cf1766811d791"
 
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:2222"
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[127.0.0.1]:2222"
 
 docker stop "${CONTAINER_NAME}"
 docker rm "${CONTAINER_NAME}"
